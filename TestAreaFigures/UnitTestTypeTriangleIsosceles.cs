a6dd2bc99fdee6f36db84f28db511e124a6b04c3
indeeezzz <indeeezzz@hotmail.com>
2023-10-28 22:33:02 +0300

    Тест кодировки

--- TestAreaFigures/UnitTestTypeTriangleIsosceles.cs
+++ TestAreaFigures/UnitTestTypeTriangleIsosceles.cs
@@ -5,20 +5,19 @@ namespace TestAreaFigures
     [TestClass]
     public class UnitTestTypeTriangleIsosceles
     {
-        string TriangleName = "Треугольник равнобедренный";
         [TestMethod]
         public void Test_1()
         {
             Triangle triangle = new Triangle();
             string result = triangle.typeTriangleOnThSides(9, 9, 8);
-            Assert.AreEqual(TriangleName, result);
+            Assert.AreEqual("Треугольник равнобедренный", result);
         }
         [TestMethod]
         public void Test_2()
         {
             Triangle triangle = new Triangle(9, 9, 8);
             string result = triangle.typeTriangleOnThSides();
-            Assert.AreEqual(TriangleName, result);
+            Assert.AreEqual("Треугольник равнобедренный", result);
         }
 
     }
