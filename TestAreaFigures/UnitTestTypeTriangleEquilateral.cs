80e87b234ad76c4a5a1e7c8f161e4cd49da8c0c3
indeeezzz <indeeezzz@hotmail.com>
2023-10-28 22:03:02 +0300

    Тест кодировки UTF-8

--- TestAreaFigures/UnitTestTypeTriangleEquilateral.cs
+++ TestAreaFigures/UnitTestTypeTriangleEquilateral.cs
@@ -1,4 +1,5 @@
 using GeometryLibrary;
+using System.Text;
 
 namespace TestAreaFigures
 {
@@ -10,14 +11,14 @@ namespace TestAreaFigures
         {
             Triangle triangle = new Triangle();
             string result = triangle.typeTriangleOnThSides(8, 8, 8);
-            Assert.AreEqual("Треугольник равносторонний", result);
+            Assert.AreEqual($"Треугольник равносторонний", result);
         }
         [TestMethod]
         public void Test_2()
         {
             Triangle triangle = new Triangle(8, 8, 8);
             string result = triangle.typeTriangleOnThSides();
-            Assert.AreEqual("Треугольник равносторонний", result);
+            Assert.AreEqual($"Треугольник равносторонний", result);
         }
 
     }
